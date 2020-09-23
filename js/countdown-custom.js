0cee4a7acdeb16b2af0bde842e471166e3537f9d
Mudit Lal <46276282+Mudit-Lal@users.noreply.github.com>
2020-09-23 15:31:46 +0530

    updated countdown

--- js/countdown-custom.js
+++ js/countdown-custom.js
@@ -1,6 +1,5 @@
 jQuery(document).ready(function() {
         $(function () {
-            $('#defaultCountdown').countdown({until: new Date(2021, 03, 20, 8)}); // year, month, date, hour
+            $('#defaultCountdown').countdown({until: new Date(2020, 10, 16, 8)}); // year, month, date, hour
         });
-});		
-
+});
