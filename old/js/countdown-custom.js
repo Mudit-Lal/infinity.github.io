2a084051f66090df8603dfdba8c789236bab280c
Mudit Lal <46276282+Mudit-Lal@users.noreply.github.com>
2020-10-15 09:44:14 +0530

    Update 3

---
+++ old/js/countdown-custom.js
@@ -0,0 +1,5 @@
+jQuery(document).ready(function() {
+        $(function () {
+            $('#defaultCountdown').countdown({until: new Date(2020, 9, 16, 8)}); // year, month, date, hour
+        });
+});
